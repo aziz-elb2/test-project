44812e600a1c342bd33be2ab96c7113e484bf0a6
Aziz El Abbassi <azizelabbassi5@gmail.com>
2025-08-06 21:54:16 +0100

    the 4 operations are added

--- script.py
+++ script.py
@@ -3,9 +3,19 @@ print("Hello World!")
 
 
 def Sum(a,b,operation="add"):
-    if operation == "subs":
+    if operation == "sub":
         return a-b
-    return a+b
+    elif operation == "mul":
+        return a*b
+    elif operation == "div":
+        return a/b
+    else:
+        return a+b
 
-print("Sum : ",Sum(4,5))
-print("Sum : ",Sum(4,5,"subs"))
+a,b = 4,5
+print("Test other operations ......")
+print(f"a = {a} , b = {b}")
+print("Sum : ",Sum(a,b,"add"))
+print("Mul : ",Sum(a,b,"mul"))
+print("Div : ",Sum(a,b,"div"))
+print("Sub : ",Sum(a,b,"sub"))
